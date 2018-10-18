7a50fdd132f59f5c3e46b020c5b8483f44fe0afd
Bogdan <barjaktarevicbogdan@gmail.com>
2018-10-18 22:14:45 +0200

    Updated Menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import classes from './Layout.module.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import React, { Component } from "react";
+import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => {
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+
+  render() {
     return (
-        <>
+      <>
+        <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
         <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-        </>
-    )
+        <main className={classes.Content}>{this.props.children}</main>
+      </>
+    );
+  }
 }
 
 export default Layout;
